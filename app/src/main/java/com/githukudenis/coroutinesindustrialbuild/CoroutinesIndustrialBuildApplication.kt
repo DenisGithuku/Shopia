9aac2b64715a2d90b6221cc9f5f91992e77fda35
githukudenis <githukudenis@gmail.com>
2023-03-18 15:33:23 +0300

    change refresh product worker to a one time request type

--- app/src/main/java/com/githukudenis/coroutinesindustrialbuild/CoroutinesIndustrialBuildApplication.kt
+++ app/src/main/java/com/githukudenis/coroutinesindustrialbuild/CoroutinesIndustrialBuildApplication.kt
@@ -3,11 +3,12 @@ package com.githukudenis.coroutinesindustrialbuild
 import android.app.Application
 import androidx.work.Constraints
 import androidx.work.NetworkType
-import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.githukudenis.feature_product.workers.RefreshProductsWorker
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import java.util.Calendar
 import java.util.concurrent.TimeUnit
 
 @HiltAndroidApp
@@ -18,16 +19,31 @@ class CoroutinesIndustrialBuildApp : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
+
+    }
+
+    private fun setUpWork() {
+        val currentDate = Calendar.getInstance()
+        val dueDate = Calendar.getInstance()
+
+        //set execution to be around 8:00 AM
+        dueDate.set(Calendar.HOUR_OF_DAY, 8)
+        dueDate.set(Calendar.MINUTE, 0)
+        dueDate.set(Calendar.MINUTE, 0)
+
+        if (dueDate.before(currentDate)) {
+            dueDate.add(Calendar.HOUR_OF_DAY, 24)
+        }
+        val timeDifference = dueDate.timeInMillis - currentDate.timeInMillis
+
         val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED)
             .setRequiresBatteryNotLow(true).setRequiresDeviceIdle(true).build()
 
         val refreshProductsWorkRequest =
-            PeriodicWorkRequestBuilder<RefreshProductsWorker>(12, TimeUnit.HOURS).setConstraints(
-                    constraints
-                ).build()
+            OneTimeWorkRequestBuilder<RefreshProductsWorker>().setConstraints(
+                constraints
+            ).setInitialDelay(timeDifference, TimeUnit.MILLISECONDS).build()
 
-        WorkManager.getInstance(this)
-            .enqueue(refreshProductsWorkRequest)
-
+        WorkManager.getInstance(this).enqueue(refreshProductsWorkRequest)
     }
 }
