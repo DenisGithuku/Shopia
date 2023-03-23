b2aa15bacf3a4246108ed39e98c0487bcf556871
githukudenis <githukudenis@gmail.com>
2023-03-23 18:22:43 +0300

    insert product into cart test

--- core_data/src/main/java/com/githukudenis/core_data/data/local/db/CartDao.kt
+++ core_data/src/main/java/com/githukudenis/core_data/data/local/db/CartDao.kt
@@ -3,10 +3,17 @@ package com.githukudenis.core_data.data.local.db
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.githukudenis.core_data.data.local.db.model.cart.Product
 
 @Dao
 interface CartDao {
     @Insert(onConflict = OnConflictStrategy.Companion.REPLACE)
     suspend fun insertProducts(products: List<Product>)
+
+    @Query("SELECT * FROM cart ORDER BY productId")
+    suspend fun getAllProducts(): List<Product>
+
+    @Query("SELECT COUNT(productId) FROM cart")
+    suspend fun getProductCount(): Int
 }
