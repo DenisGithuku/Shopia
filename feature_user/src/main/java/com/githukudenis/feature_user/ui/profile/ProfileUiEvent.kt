f80d102e100685b37d59a536b28c8185bc8ac38a
githukudenis <githukudenis@gmail.com>
2023-03-28 14:24:37 +0300

    add loading spinner to profile on load

--- feature_user/src/main/java/com/githukudenis/feature_user/ui/profile/ProfileUiEvent.kt
+++ feature_user/src/main/java/com/githukudenis/feature_user/ui/profile/ProfileUiEvent.kt
@@ -2,4 +2,6 @@ package com.githukudenis.feature_user.ui.profile
 
 sealed class ProfileUiEvent {
     object Logout: ProfileUiEvent()
+
+    class DismissUserMessage(val messageId: Int): ProfileUiEvent()
 }
