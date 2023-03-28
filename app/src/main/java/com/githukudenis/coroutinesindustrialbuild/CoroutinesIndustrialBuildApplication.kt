a2fd450ccb3e876946e51ee97bd5b060ce0a3697
githukudenis <githukudenis@gmail.com>
2023-03-28 12:55:43 +0300

    move refresh products worker from application class to main activity

--- app/src/main/java/com/githukudenis/coroutinesindustrialbuild/CoroutinesIndustrialBuildApplication.kt
+++ app/src/main/java/com/githukudenis/coroutinesindustrialbuild/CoroutinesIndustrialBuildApplication.kt
@@ -1,15 +1,8 @@
 package com.githukudenis.coroutinesindustrialbuild
 
 import android.app.Application
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import com.githukudenis.feature_product.workers.RefreshProductsWorker
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import java.util.Calendar
-import java.util.concurrent.TimeUnit
 
 @HiltAndroidApp
 class CoroutinesIndustrialBuildApp : Application() {
@@ -18,31 +11,5 @@ class CoroutinesIndustrialBuildApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        setUpWork()
-    }
-
-    private fun setUpWork() {
-        val currentDate = Calendar.getInstance()
-        val dueDate = Calendar.getInstance()
-
-        //set execution to be around 8:00 AM
-        dueDate.set(Calendar.HOUR_OF_DAY, 8)
-        dueDate.set(Calendar.MINUTE, 0)
-        dueDate.set(Calendar.MINUTE, 0)
-
-        if (dueDate.before(currentDate)) {
-            dueDate.add(Calendar.HOUR_OF_DAY, 24)
-        }
-        val timeDifference = dueDate.timeInMillis - currentDate.timeInMillis
-
-        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED)
-            .setRequiresBatteryNotLow(true).setRequiresDeviceIdle(true).build()
-
-        val refreshProductsWorkRequest =
-            OneTimeWorkRequestBuilder<RefreshProductsWorker>().setConstraints(
-                constraints
-            ).setInitialDelay(timeDifference, TimeUnit.MILLISECONDS).build()
-
-        WorkManager.getInstance(this).enqueue(refreshProductsWorkRequest)
     }
 }
--- app/src/main/java/com/githukudenis/coroutinesindustrialbuild/MainActivity.kt
+++ app/src/main/java/com/githukudenis/coroutinesindustrialbuild/MainActivity.kt
@@ -17,10 +17,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.githukudenis.core_data.util.Constants
 import com.githukudenis.core_design.theme.CoroutinesIndustrialBuildTheme
 import com.githukudenis.feature_product.ui.util.AppDestination
+import com.githukudenis.feature_product.workers.RefreshProductsWorker
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -76,6 +85,22 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        WorkManager.getInstance(this).getWorkInfosByTagLiveData(Constants.products_sync_worker)
+            .observe(this) { workInfoList ->
+                if (workInfoList.isNotEmpty()) {
+                    val productsWorker = workInfoList.find { workInfo ->
+                        workInfo.tags.contains(Constants.products_sync_worker)
+                    }
+                    productsWorker?.let { workInfo ->
+                        if (workInfo.state != WorkInfo.State.ENQUEUED) {
+                            setUpProductsSyncWork()
+                        }
+                    }
+                } else {
+                    setUpProductsSyncWork()
+                }
+            }
     }
 
     override fun onStart() {
@@ -83,6 +108,41 @@ class MainActivity : ComponentActivity() {
         val appStarted = mainViewModel.uiState.value.appStarted
         if (!appStarted) {
             mainViewModel.updateAppStartStatus(true)
+        }
+    }
+
+    private fun setUpProductsSyncWork() {
+        val currentDate = Calendar.getInstance()
+        val dueDate = Calendar.getInstance()
+
+        //set execution to be around 8:00 AM
+        dueDate.set(Calendar.HOUR_OF_DAY, 8)
+        dueDate.set(Calendar.MINUTE, 0)
+        dueDate.set(Calendar.MINUTE, 0)
+
+        if (dueDate.before(currentDate)) {
+            dueDate.add(Calendar.HOUR_OF_DAY, 24)
+        }
+        val timeDifference = dueDate.timeInMillis - currentDate.timeInMillis
+
+        val constraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true).setRequiresDeviceIdle(true).build()
+
+        val refreshProductsWorkRequest =
+            OneTimeWorkRequestBuilder<RefreshProductsWorker>().setConstraints(
+                constraints
+            ).setInitialDelay(timeDifference, TimeUnit.MILLISECONDS)
+                .addTag(Constants.products_sync_worker).build()
+
+        WorkManager.getInstance(this).enqueue(refreshProductsWorkRequest)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val workInfo =
+            WorkManager.getInstance(this).getWorkInfosByTagLiveData(Constants.products_sync_worker)
+        if (workInfo.hasActiveObservers()) {
+            workInfo.removeObservers(this)
         }
     }
 }
