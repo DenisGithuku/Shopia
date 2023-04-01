3c91c7e179e91225642ac59c942488052d70a0b1
githukudenis <githukudenis@gmail.com>
2023-04-01 13:28:13 +0300

    add add product row

--- feature_product/src/main/java/com/githukudenis/feature_product/ui/views/detail/ProductDetailScreen.kt
+++ feature_product/src/main/java/com/githukudenis/feature_product/ui/views/detail/ProductDetailScreen.kt
@@ -1,28 +1,42 @@
 package com.githukudenis.feature_product.ui.views.detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Difference
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
@@ -30,6 +44,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -50,8 +65,7 @@ fun ProductDetailScreen(
 
     if (state.isLoading) {
         Box(
-            modifier = modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center
         ) {
             CircularProgressIndicator()
         }
@@ -61,11 +75,10 @@ fun ProductDetailScreen(
     LaunchedEffect(key1 = state) {
         if (state.userMessages.isNotEmpty()) {
             val (id, userMessage) = state.userMessages[0]
-            userMessage?.let{ message ->
+            userMessage?.let { message ->
                 scope.launch {
                     snackbarHostState.showSnackbar(
-                        message  = message,
-                        duration = SnackbarDuration.Long
+                        message = message, duration = SnackbarDuration.Long
                     )
                     id?.let {
                         productsDetailViewModel.onEvent(ProductDetailEvent.DismissUserMessage(it))
@@ -94,20 +107,15 @@ fun ProductDetailScreen(
             contentScale = ContentScale.Fit
         )
 
-        Text(
-            modifier = modifier,
+        Text(modifier = modifier,
             text = title?.replaceFirstChar { char -> char.uppercase() } ?: "",
             style = TextStyle(
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp
-            )
-        )
+                fontWeight = FontWeight.Bold, fontSize = 20.sp
+            ))
         Text(
-            text = description ?: "",
-            textAlign = TextAlign.Justify
+            text = description ?: "", textAlign = TextAlign.Justify
         )
-        Text(
-            text = price ?: "0",
+        Text(text = price ?: "0",
             textAlign = TextAlign.End,
             color = Color.White,
             modifier = modifier
@@ -117,16 +125,14 @@ fun ProductDetailScreen(
                         cornerRadius = CornerRadius(x = 32.dp.toPx(), y = 32.dp.toPx())
                     )
                 }
-                .padding(12.dp)
-        )
+                .padding(12.dp))
 
         Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(6.dp)) {
             Text(
                 text = "Rated: "
             )
             Row(
-                modifier = modifier,
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
+                modifier = modifier, horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
 
                 rating?.let { rating ->
@@ -141,5 +147,67 @@ fun ProductDetailScreen(
             }
         }
     }
+}
 
+@Composable
+fun CartSection(
+    modifier: Modifier = Modifier, onAddToCart: (Int) -> Unit
+) {
+    var productCount by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    var buttonEnabled = remember {
+        derivedStateOf {
+            productCount >= 1
+        }
+    }
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            modifier = modifier, onClick = {
+                onAddToCart(productCount)
+            }, enabled = buttonEnabled.value
+
+        ) {
+            Text(
+                "Add to cart"
+            )
+        }
+        Row(
+            modifier = modifier
+        ) {
+            Box(
+                modifier = modifier.size(30.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = MaterialTheme.colors.secondary)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add one",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+            Box(
+                modifier = modifier.size(30.dp)
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = MaterialTheme.colors.secondary)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Difference,
+                    contentDescription = "Minus one",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CartSectionPreview() {
+    CartSection(onAddToCart = {})
 }
